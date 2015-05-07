38aec0942e60a2e23baabd83e218a55c0e3ea360
Ray van B <deslaper@gmail.com>
2015-05-07 11:00:15 +0200

    Added a simple disclaimer

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -18,6 +18,11 @@
 
     @yield('content')
 </div>
+<footer class="footer">
+    <div class="container">
+      <p class="text-muted">The administrator of this tool holds no responsibility for the content of any linked website(s), files(s) nor any other linked content.</p>
+    </div>
+</footer>
 
 <script src="//code.jquery.com/jquery.min.js"></script>
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
