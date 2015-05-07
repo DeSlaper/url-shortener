69b15f472d4da49fa3ed143ee049c140e4e2cfd3
Ray van B <deslaper@gmail.com>
2015-05-07 11:05:42 +0200

    Minor changes

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>URL Shortner</title>
+    <title>URL Shortener | myadm.nl</title>
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css"/>
     <link rel="stylesheet" href="/assets/css/style.css"/>
 </head>
